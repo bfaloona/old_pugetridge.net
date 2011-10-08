f9336a9c5cf51ce6a512fad95f6b725285612a3c
Brandon Faloona <brandon@faloona.net>
2011-10-08 12:32:13 -0700

    Initial commit

---
+++ public_html/hiddenemail.js
@@ -0,0 +1,4 @@
+var user = "prcatours"; 
+var host = "hotmail.com"; 
+var link = "Email"; 
+document.write("<a hre" + "f=ma" + "ilto:" + user + "@" + host + ">" + link + "</a>"); 
---
+++ public_html/hiddeninterestemail.js
@@ -0,0 +1,4 @@
+var user = "interest_list"; 
+var host = "pugetridge.net"; 
+var link = "Email"; 
+document.write("<a hre" + "f=ma" + "ilto:" + user + "@" + host + ">" + link + "</a>"); 
